3fe8f45f495fb3b4d2df39db6c11327796c5836d
yca452 <yca452@sfu.ca>
2024-01-27 06:27:23 -0800

    Modified card linking

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -9,24 +9,24 @@ import Link from '@mui/material/Link';
 const ActionAreaCard = ( { title, authors, date, imageURL, link } ) => {
   return (
     <Card sx={{ maxWidth: 345, padding: "20px", marginLeft: "20px", backgroundColor: "#f0f0f0"}}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={imageURL}
-          alt="card"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5">
-            <Link href={link} underline="none" style={{ color: "#831324", fontWeight: "bold"}}>
-              {title}
-            </Link>
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {authors} | {date}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <Link href={link} underline="none" style={{ color: "#831324", fontWeight: "bold"}}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={imageURL}
+            alt="card"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5">
+                {title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {authors} | {date}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Link>
     </Card>
   );
 }
